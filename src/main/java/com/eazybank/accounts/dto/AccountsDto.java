cfda0dfdaa10a762a1a2e46143afe226e73f442c
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-02-21 02:16:38 +0530

    perform data validations

--- src/main/java/com/eazybank/accounts/dto/AccountsDto.java
+++ src/main/java/com/eazybank/accounts/dto/AccountsDto.java
@@ -1,12 +1,19 @@
 package com.eazybank.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
+
+    @NotEmpty(message = "Account Number cannot be a null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "Account Type cannot be a null or empty")
     private String accountType;
 
+    @NotEmpty(message = "Branch Address cannot be a null or empty")
     private String branchAddress;
 }
