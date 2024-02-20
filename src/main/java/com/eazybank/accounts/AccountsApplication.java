1b7ba06600b5026167aa3904809326faf67bcaec
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-02-21 02:46:47 +0530

    Enhanced documentation using @OpenAPIDocumentation

--- src/main/java/com/eazybank/accounts/AccountsApplication.java
+++ src/main/java/com/eazybank/accounts/AccountsApplication.java
@@ -1,11 +1,33 @@
 package com.eazybank.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(info = @Info(
+        title = "EazyBank Accounts microservice OpenAPI Documentation",
+        description = "EazyBank Accounts microservice REST API Documentation",
+        version = "v1",
+        contact = @Contact(
+                name = "Shahrukh Tramboo",
+                email = "mail@clayster.net",
+                url = "https://www.clayster.net"
+        ),
+        license = @License(
+                name = "Apache 2.0",
+                url = "https://www.clayster.net"
+        )
+), externalDocs = @ExternalDocumentation(
+        description = "EazyBak Accounts microservice REST API Documentation",
+        url = "https://www.clayster.net"
+))
 public class AccountsApplication {
 
     public static void main(String[] args) {
