d5854640571c9787285bd597322f3f21f749384a
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-02-22 22:26:05 +0530

    update docker compose file and add liveness and readiness probes and optimizing docker compose file

--- src/main/java/com/eazybank/accounts/mapper/AccountsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.eazybank.accounts.mapper;
-
-import com.eazybank.accounts.dto.AccountsDto;
-import com.eazybank.accounts.entity.Accounts;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface AccountsMapper extends EntityMapper<AccountsDto, Accounts> {
-}
--- src/main/java/com/eazybank/accounts/mapper/CustomerMapper.java
+++
@@ -1,9 +0,0 @@
-package com.eazybank.accounts.mapper;
-
-import com.eazybank.accounts.dto.CustomerDto;
-import com.eazybank.accounts.entity.Customer;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface CustomerMapper extends EntityMapper<CustomerDto, Customer> {
-}
