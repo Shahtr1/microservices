92167f327c5b740fe8310be6135bd0599a96f7eb
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-02-24 11:16:13 +0530

    implement retry pattern in gateway

--- gatewayserver/src/main/java/com/eazybank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazybank/gatewayserver/GatewayserverApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -33,6 +35,13 @@ public class GatewayserverApplication {
 						.filters(
 								f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}")
 										.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+										.retry(retryConfig -> retryConfig.setRetries(3)
+												.setMethods(HttpMethod.GET)
+												// it will wait max 1000 ms between retries
+												.setBackoff(
+														Duration.ofMillis(100),
+														Duration.ofMillis(1000),
+														2,true))
 						)
 						.uri("lb://LOANS"))
 				.route(p -> p
