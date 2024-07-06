c561b487242238657faab51460d9a0ae5f59fc81
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-07-06 13:25:53 +0530

    Added required microservices for SAGA implementation

---
+++ event driven - saga/OrderService/src/main/java/com/shahbytes/OrderService/command/api/aggregate/OrderAggregate.java
@@ -0,0 +1,45 @@
+package com.shahbytes.OrderService.command.api.aggregate;
+
+import com.shahbytes.OrderService.command.api.commands.CreateOrderCommand;
+import com.shahbytes.OrderService.command.api.events.OrderCreatedEvent;
+import lombok.NoArgsConstructor;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
+
+@Aggregate
+@NoArgsConstructor
+public class OrderAggregate {
+
+    @AggregateIdentifier
+    private String orderId;
+    private String productId;
+    private String userId;
+    private String addressId;
+    private Integer quantity;
+    private String orderStatus;
+
+    @CommandHandler
+    public OrderAggregate(CreateOrderCommand command) {
+        // validate the command
+        OrderCreatedEvent orderCreatedEvent = new OrderCreatedEvent();
+        BeanUtils.copyProperties(command, orderCreatedEvent);
+
+        // create the event
+        AggregateLifecycle.apply(orderCreatedEvent);
+    }
+
+    // whatever event is created, we are updating it in Aggregate too
+    @EventSourcingHandler
+    public void on(OrderCreatedEvent event) {
+        this.orderId = event.getOrderId();
+        this.productId = event.getProductId();
+        this.userId = event.getUserId();
+        this.addressId = event.getAddressId();
+        this.quantity = event.getQuantity();
+        this.orderStatus = event.getOrderStatus();
+    }
+}
